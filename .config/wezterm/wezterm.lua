e0f6db778e61ae05eec8e9412520face41f6eb55
adtran83 <adtran83@gmail.com>
2025-04-08 01:07:42 +0200

    Use tokyo-night theme (wezterm & starship) + Nerd Fond

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,7 +6,10 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
-config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font_with_fallback({
+	"JetBrainsMono Nerd Font",
+	"Noto Sans",
+})
 config.font_size = 18.0
 
 config.enable_tab_bar = false
@@ -15,7 +18,8 @@ config.window_background_opacity = 0.85
 config.macos_window_background_blur = 10
 
 -- For example, changing the color scheme:
-config.color_scheme = "Gruvbox Dark (Gogh)"
+-- config.color_scheme = "Gruvbox Dark (Gogh)"
+config.color_scheme = "tokyonight_night"
 
 config.send_composed_key_when_left_alt_is_pressed = false
 config.send_composed_key_when_right_alt_is_pressed = true
