d0488c8d0c52f5844722435c588cbbea2fc86347
adtran83 <adtran83@gmail.com>
2025-04-09 00:46:07 +0200

    feat(theme): add gruvbox and configure transparency
    - Add gruvbox.nvim as an alternative colorscheme
    - Configure tokyonight theme with transparency settings for sidebars
      and floats
    - Remove undotree plugin as it's no longer needed

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
